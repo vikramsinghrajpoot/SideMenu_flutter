473a51fccb949bf8500e1d943ecf3ca6070c984a
vikram <vikram_rajpoot@external.mckinsey.com>
2020-03-13 21:11:45 +0530

    Routing

--- lib/Modules/Screens/Login/LoginScreen.dart
+++ lib/Modules/Screens/Login/LoginScreen.dart
@@ -5,8 +5,57 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Center(child: Text('LoginScreen')),
+        child: Center(
+            child: Column(
+          children: <Widget>[
+            Text('LoginScreen'),
+            Image.asset('images/appicon.png'),
+            Image.asset(
+              'images/user.png',
+              height: 60,
+            ),
+            FlatButton(
+              onPressed: () => this._clicked(Type.one, context),
+              child: Text('oneTitle'),
+            ),
+            FlatButton(
+              onPressed: () => this._clicked(Type.two, context),
+              child: Text('twoTitle'),
+            ),
+            FlatButton(
+              onPressed: () => this._clicked(Type.three, context),
+              child: Text('threeTitle'),
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  color: Colors.red,
+                  height: 40,
+                  width: 40,
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20),
+                  color: Colors.green,
+                  height: 40,
+                  width: 40,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 40,
+                  width: 40,
+                )
+              ],
+            )
+          ],
+        )),
       ),
     );
   }
+
+  _clicked(Type type, context) {
+    print(type);
+    Navigator.pushNamed(context, '/notes');
+  }
 }
+
+enum Type { one, two, three }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:notes_rea/Modules/Athuntication/Authentication.dart';
+import 'package:notes_rea/Modules/Screens/Login/LoginScreen.dart';
+import 'package:notes_rea/Modules/Screens/Notes/Notes/NotesScreen.dart';
+import 'package:notes_rea/Modules/Screens/Settings/Settings.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,13 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Authentication(),
+      // home: Authentication(),
+      routes: {
+        '/': (context) => Authentication(),
+        '/notes': (context) => NotesScreen(),
+        '/settings': (context) => Settings(),
+        '/login': (context) => LoginScreen(),
+      },
     );
   }
 }
-
-
